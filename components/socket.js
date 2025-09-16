c65c9856fe4a6756fca6036c25a7fbbf67b1bb33
stevebradram <ramdevelopmentinfo@gmail.com>
2025-09-16 12:53:28 +0300

    Update socket.js

--- components/socket.js
+++ components/socket.js
@@ -1,3 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001");
+export const socket = io("http://localhost:3001", {
+extraHeaders: {
+‘Access-Control-Allow-Origin’: ‘*’
+}
+});
