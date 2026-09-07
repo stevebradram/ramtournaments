43aa5225cfd8aa1b1de4acb2eab91aabb51a10a7
stevebradram <ramdevelopmentinfo@gmail.com>
2026-09-07 14:27:53 -0400

    Use local NFL team data for logos instead of ESPN API

--- components/Event/NFLRegModal.js
+++ components/Event/NFLRegModal.js
@@ -11,6 +11,42 @@ import theRamOdds from './ramOdds.json'
 import theNFLOdds from '../TheJSONS/nflOdds.json'
 import localStorage from 'local-storage'
 
+const NFL_TEAM_ROWS = [
+  ['Arizona Cardinals', 'Cardinals', 'ari'],
+  ['Atlanta Falcons', 'Falcons', 'atl'],
+  ['Baltimore Ravens', 'Ravens', 'bal'],
+  ['Buffalo Bills', 'Bills', 'buf'],
+  ['Carolina Panthers', 'Panthers', 'car'],
+  ['Chicago Bears', 'Bears', 'chi'],
+  ['Cincinnati Bengals', 'Bengals', 'cin'],
+  ['Cleveland Browns', 'Browns', 'cle'],
+  ['Dallas Cowboys', 'Cowboys', 'dal'],
+  ['Denver Broncos', 'Broncos', 'den'],
+  ['Detroit Lions', 'Lions', 'det'],
+  ['Green Bay Packers', 'Packers', 'gb'],
+  ['Houston Texans', 'Texans', 'hou'],
+  ['Indianapolis Colts', 'Colts', 'ind'],
+  ['Jacksonville Jaguars', 'Jaguars', 'jax'],
+  ['Kansas City Chiefs', 'Chiefs', 'kc'],
+  ['Las Vegas Raiders', 'Raiders', 'lv'],
+  ['Los Angeles Chargers', 'Chargers', 'lac'],
+  ['Los Angeles Rams', 'Rams', 'lar'],
+  ['Miami Dolphins', 'Dolphins', 'mia'],
+  ['Minnesota Vikings', 'Vikings', 'min'],
+  ['New England Patriots', 'Patriots', 'ne'],
+  ['New Orleans Saints', 'Saints', 'no'],
+  ['New York Giants', 'Giants', 'nyg'],
+  ['New York Jets', 'Jets', 'nyj'],
+  ['Philadelphia Eagles', 'Eagles', 'phi'],
+  ['Pittsburgh Steelers', 'Steelers', 'pit'],
+  ['San Francisco 49ers', '49ers', 'sf'],
+  ['Seattle Seahawks', 'Seahawks', 'sea'],
+  ['Tampa Bay Buccaneers', 'Buccaneers', 'tb'],
+  ['Tennessee Titans', 'Titans', 'ten'],
+  ['Washington Commanders', 'Commanders', 'wsh'],
+]
+const NFL_TEAMS_DATA = [{ leagues: [{ teams: NFL_TEAM_ROWS.map(r => ({ team: { displayName: r[0], nickname: r[1], logos: [{ href: 'https://a.espncdn.com/i/teamlogos/nfl/500/' + r[2] + '.png' }] } })) }] }]
+
 var week1RoundEdit = [], week2RoundEdit = [], week3RoundEdit = [], superBowlEdit = [], week4RoundEdit = []
 
 var week1RoundEdit2 = [], week2RoundEdit2 = [], week3RoundEdit2 = [], superBowlEdit2 = [], week4RoundEdit2 = []
@@ -490,7 +526,7 @@ class NCAAModal extends Component {
     //const response = await axios.get(logosUrl);
     //console.log(response.data);
     var smallResultsArr = []
-    axios.get(logosUrl)
+    Promise.resolve({ data: { sports: NFL_TEAMS_DATA } })
       .then((res) => {
         var resultsArr = res.data['sports']
         //console.log('the logos 1111', resultsArr.length)
